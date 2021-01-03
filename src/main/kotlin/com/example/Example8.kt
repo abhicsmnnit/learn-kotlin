2a95605821e50a623e1f02090ebcb8a121a60ef7
Abhinav Verma <abhicsmnnit@gmail.com>
2021-01-03 09:27:42 +0530

    Using `when` without an argument

--- src/main/kotlin/com/example/Example8.kt
+++ src/main/kotlin/com/example/Example8.kt
@@ -13,7 +13,11 @@ fun main() {
 
     println(mix(RED, YELLOW))
     println(mix(YELLOW, RED))
-    println(mix(RED, BLUE))
+    // println(mix(RED, BLUE)) -> Exception in thread "main" java.lang.Exception: Dirty color
+
+    println(mixOptimized(RED, YELLOW))
+    println(mixOptimized(YELLOW, RED))
+    // println(mixOptimized(RED, BLUE)) -> Exception in thread "main" java.lang.Exception: Dirty color
 }
 
 fun getMnemonic(c: Color) = // `when` is an expression - can be returned directly from a function
@@ -39,5 +43,13 @@ fun mix(c1: Color, c2: Color) =
         setOf(RED, YELLOW) -> ORANGE // Argument is checked for equality with the branch conditions
         setOf(BLUE, YELLOW) -> GREEN
         setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
+fun mixOptimized(c1: Color, c2: Color) = // Optimized because it creates no instances of `Set`
+    when { // No arguments for `when`
+        (c1 == RED && c2 == YELLOW) || (c1 == YELLOW && c2 == RED) -> ORANGE
+        (c1 == BLUE && c2 == YELLOW) || (c1 == YELLOW && c2 == BLUE) -> GREEN
+        (c1 == BLUE && c2 == VIOLET) || (c1 == VIOLET && c2 == BLUE) -> INDIGO
         else -> throw Exception("Dirty color")
     }
